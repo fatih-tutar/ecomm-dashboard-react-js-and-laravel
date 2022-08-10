3ec0e9fbb239e5088704a4a65ba18f5e49d52b85
fatih <fatih.tutar@dijitalvarliklar.com.tr>
2022-08-10 11:20:02 +0300

    Add Routing in Project

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import {Container, Nav, Navbar} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+
+function Header()
+{
+    return(
+        <div>
+            <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">E-Comm </Navbar.Brand>
+                <Nav className="me-auto navbar_wrapper">
+                    <Link to="/add">Add Products</Link>
+                    <Link to="/update">Update Products</Link>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                </Nav>
+            </Container>
+        </Navbar>
+        </div>
+    )
+}
+
+export default Header  
